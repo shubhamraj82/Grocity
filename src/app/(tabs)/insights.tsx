362636baadde28e804156a7b46102681b385a0de
shubhamraj82 <raj.shubh8216@gmail.com>
2026-08-01 21:37:49 +0530

    Integrate Sentry for error tracking and feedback. Added @sentry/react-native dependency, initialized Sentry in the app layout, and included a feedback button in the insights screen. Updated .gitignore to include .env.local and modified app.json for Sentry configuration.

--- src/app/(tabs)/insights.tsx
+++ src/app/(tabs)/insights.tsx
@@ -1,12 +1,33 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView } from 'react-native'
+import SentryFeedbackButton from '@/components/insights/SentryFeedbackButton';
+import InsightsPrioritySection from '@/components/insights/InsightsPrioritySection';
+import ClearCompletedButton from '@/components/insights/ClearCompletedButton';
+import UserProfile from '@/components/insights/UserProfile';
+import InsightsCategorySection from '@/components/insights/InsightsCategorySection';
+import InsightsStatsSection from '@/components/insights/InsightsStatsSection';
+import TabScreenBackground from '@/components/TabScreenBackground';
 
 const insightsScreen = () => {
   return (
-    <View>
-      <Text>insightsScreen</Text>
-    </View>
-  )
-}
+    <>
+      <ScrollView
+        className="flex-1 bg-background py-4"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ padding: 20, gap: 14 }}
+        contentInsetAdjustmentBehavior="automatic"
+      >
+        <TabScreenBackground />
 
-export default insightsScreen
+        <UserProfile />
+        <InsightsStatsSection />
+        <InsightsCategorySection />
+        <InsightsPrioritySection />
+        <ClearCompletedButton />
+      </ScrollView>
+
+      <SentryFeedbackButton />
+    </>
+  );
+};
+
+export default insightsScreen;  
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,21 +2,30 @@ import { ClerkProvider } from "@clerk/expo";
 import { tokenCache } from "@clerk/expo/token-cache";
 import { DarkTheme, DefaultTheme, Stack, ThemeProvider } from "expo-router";
 import "../../global.css";
+import * as Sentry from "@sentry/react-native";
 import { useColorScheme } from "react-native";
+import { KeyboardProvider } from "react-native-keyboard-controller";
 
 const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY ?? "";
 
 if (!publishableKey) {
   throw new Error("Add EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY to your .env file");
 }
+Sentry.init({
+  dsn: process.env.EXPO_PUBLIC_SENTRY_DSN,
+  integrations: [Sentry.feedbackIntegration()],
+});
+
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   return (
     <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
-      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack screenOptions={{ headerShown: false }} />
-      </ThemeProvider>
+           <KeyboardProvider>
+        <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+          <Stack screenOptions={{ headerShown: false }} />
+        </ThemeProvider>
+      </KeyboardProvider>
     </ClerkProvider>
   );
 }
